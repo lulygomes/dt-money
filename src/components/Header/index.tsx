9b42231f6f4babaefd333bccc03119819d698cda
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-09-10 00:34:58 -0300

    modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,61 @@
+import { useCallback, useState } from 'react';
+import Modal from 'react-modal';
 
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 import logoImg from '../../assets/logo.svg'
 
-import { Container, Content, Button } from './styles'
+import { Container, Content, Button, customStyles, ModalContent } from './styles'
 
 export function Header() {
+  const [modalIsOpen, setIsOpen ] = useState(false)
+
+  const openModal = useCallback(()=> {
+    setIsOpen(true)
+  }, [])
+
+  const closeModal = useCallback(()=> {
+    setIsOpen(false)
+  }, [])
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <Button type="button">
+        <Button type="button" onClick={openModal}>
           Nova transação
         </Button>
+        <Modal 
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Adicionar nova transação"
+        >
+          <ModalContent>
+          <h1>Cadastrar transação</h1>
+
+          <input type="text" placeholder="Nome" />
+          <input type="text" placeholder="Preço" />
+
+          <div className="buttonBox">
+            <button>
+              <img src={incomeImg} alt="Entrada logo" />
+              Entrada
+            </button>
+            <button>
+              <img src={outcomeImg} alt="Saída logo" />
+              Saída
+            </button>
+          </div>
+          <input type="text" placeholder='Categoria' />
+
+          <button className="submit">
+            Cadastrar
+          </button>
+          </ModalContent>
+
+        </Modal>
       </Content>
     </Container>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Styles } from 'react-modal';
 
 export const Container = styled.header`
   background: var(--blue);
@@ -13,6 +14,8 @@ export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
   padding: 2rem 1rem 12rem;
+
+
 `;
 
 export const Button = styled.button`
@@ -28,5 +31,79 @@ export const Button = styled.button`
   &:hover {
     box-shadow: 0px 0px 5px 1px var(--white);
     filter: brightness(0.9);
+  }
+`;
+
+export const customStyles: Styles = {
+  overlay: {
+    background: 'rgba(0,0,0,.5)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  content: {
+    maxWidth: '576px',
+    margin: '2rem auto 0',
+    border: 'none',
+    borderRadius: '5px',
+    background: '#F0F2F5',
+  },
+};
+
+export const ModalContent = styled.section`
+  width: 100%;
+  padding: 0 1rem;
+  h1 {
+    margin-top: 1.2rem;
+    margin-bottom: .5rem;
+    letter-spacing: 1.2px;
+  }
+
+  input {
+    width: 100%;
+    height: 3.5rem;
+    margin-top: 1rem;
+    padding-left: 1.5rem;
+    border: solid 1px #d7d7d7;
+    border-radius: 0.25rem;
+    background: #E7E9EE;
+    line-height: 1.5rem;
+  }
+
+  div.buttonBox {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1rem;
+
+    button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: transparent;
+      border: solid 1px #d7d7d7;
+      border-radius: 0.25rem;
+      height: 3.5rem;
+      width: 15rem;
+
+      img {
+        height: 1.5rem;
+        margin-right: 0.50rem;
+      }
+    }
+  }
+
+  button.submit {
+    margin-top: 1.5rem;
+    width: 100%;
+    border-radius: 0.25rem;
+    border: none;
+    background: var(--green);
+    line-height: 4rem;
+
+    color: var(--white);
+    font-weight: 600;
+    font-size: 1.1rem;
+    letter-spacing: 1.2px;
   }
 `;
